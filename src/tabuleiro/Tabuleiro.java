c425e75f8ce2c7ffb2c0b751a6d65836a6dbcd93
flaviojosenunes <flaviojosenunes@gmail.com>
2021-03-10 21:20:47 -0300

    Comecando a implementar o Tabuleiro e as Pecas

---
+++ src/tabuleiro/Tabuleiro.java
@@ -0,0 +1,16 @@
+package tabuleiro;
+
+public class Tabuleiro {
+	
+	private int linhas;
+	private int colunas;
+	private Peca[][] pecas;
+	
+	public Tabuleiro(int linhas, int colunas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		pecas = new Peca[linhas][colunas];
+	}
+	
+	
+}
