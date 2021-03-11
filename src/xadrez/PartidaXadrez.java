79966271d972aeb7032549600b7b47d87881bbd1
flaviojosenunes <flaviojosenunes@gmail.com>
2021-03-11 16:06:24 -0300

    Colocando peças no Tabuleiro

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -1,6 +1,9 @@
 package xadrez;
 
+import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
+import xadrez.pecas.Rei;
+import xadrez.pecas.Torre;
 
 public class PartidaXadrez {
 
@@ -8,6 +11,7 @@ public class PartidaXadrez {
 	
 	public PartidaXadrez() {
 		tabuleiro = new Tabuleiro(8,8);
+		initialSetup();
 	}
 	
 	public PecaXadrez[][] getPecas(){
@@ -19,5 +23,11 @@ public class PartidaXadrez {
 			}
 		}
 		return mat;
+	}
+	
+	private void initialSetup() {
+		tabuleiro.colocaPeca(new Torre(tabuleiro, Cor.BRANCO), new Posicao(2,1));
+		tabuleiro.colocaPeca(new Rei(tabuleiro,Cor.PRETO), new Posicao(0,4));
+		tabuleiro.colocaPeca(new Rei(tabuleiro,Cor.BRANCO), new Posicao(7,4));
 	}
 }
